c84b04a758eb0928fc1876252ca6dc99fb07bc4d
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-08-29 18:23:03 +0600

    code pushed

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -3,7 +3,6 @@ import * as api from "../api/index"
 export const getPosts = async dispatch => {
     try {
         const { data } = await api.fetchPosts()
-        console.log(data)
         dispatch({ type: "GET_ALL", payload: data })
     } catch (error) {
         console.log(error)
