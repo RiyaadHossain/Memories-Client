6f438dde033f04cc9548ce25528c176ae99bcd90
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-08-29 10:31:08 +0600

    basic structure added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import useStyles from './style';
+import memories from "./images/memories.png"
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Memories</h1>
-    </div>
+    <Container maxWidth="lg">
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+      <img className={classes.image} src={memories} alt="icon" height="60" />
+    </AppBar>
+    <Grow in>
+      <Container>
+        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid item xs={12} sm={7}>
+            <Posts /* setCurrentId={setCurrentId} */ />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form /* currentId={currentId} setCurrentId={setCurrentId} */ />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  </Container>
   );
 }
 
